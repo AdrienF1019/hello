9d56f8223097475438fee386e10541cd1190cc05
Adrien Fortner <afortner2@huskers.unl.edu>
2020-01-16 14:13:32 -0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,12 @@
+/*  Author ~ Adrien Fortner
+* Date: January 14, 2020
+* First print program
+*/
+#include<stdio.h>
+
+int main(void){
+  printf("Adrien Fortner\n");
+  printf("Computer Engineering\n")
+  
+  return 0;
+}
